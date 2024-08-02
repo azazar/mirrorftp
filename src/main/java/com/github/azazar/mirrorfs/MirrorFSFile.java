c84c02eb49e9271a8c8a0c12fcb72ee079e759ab
Mikhail Yevchenko <m..@uo1.net>
2024-08-02 10:11:05 +0300

    Replace tabs with spaces

--- src/main/java/com/github/azazar/mirrorfs/MirrorFSFile.java
+++ src/main/java/com/github/azazar/mirrorfs/MirrorFSFile.java
@@ -21,120 +21,120 @@ import org.apache.ftpserver.ftplet.FtpFile;
  */
 public class MirrorFSFile implements FtpFile {
 
-	private static final Random RNG = new Random();
+    private static final Random RNG = new Random();
 
     String name;
     FtpFile parent;
     File[] storages;
-	private File[] storageFiles = null;
+    private File[] storageFiles = null;
 
-	MirrorFSFile(String name, MirrorFSRoot root) {
-		this.name = name;
-		this.parent = root;
-		this.storages = null;
-	}
+    MirrorFSFile(String name, MirrorFSRoot root) {
+        this.name = name;
+        this.parent = root;
+        this.storages = null;
+    }
 
-	MirrorFSFile(String name, MirrorFSFile parent) {
-		this.name = name;
-		this.parent = parent;
-		this.storages = parent.storages;
-	}
+    MirrorFSFile(String name, MirrorFSFile parent) {
+        this.name = name;
+        this.parent = parent;
+        this.storages = parent.storages;
+    }
 
-	@Override
-	public String getAbsolutePath() {
-		String parentPath = parent.getAbsolutePath();
+    @Override
+    public String getAbsolutePath() {
+        String parentPath = parent.getAbsolutePath();
 
-		if (parentPath.endsWith("/")) {
-			return parentPath + name;
-		}
+        if (parentPath.endsWith("/")) {
+            return parentPath + name;
+        }
         
-		return parentPath + "/" + name;
-	}
+        return parentPath + "/" + name;
+    }
 
-	@Override
-	public String getName() {
-		return name;
-	}
+    @Override
+    public String getName() {
+        return name;
+    }
 
-	@Override
-	public boolean isHidden() {
+    @Override
+    public boolean isHidden() {
         return false;
-	}
+    }
 
-	@Override
-	public boolean isDirectory() {
-		if (parent instanceof MirrorFSRoot)
-			return true;
-		
-		for (File path : getStorageFiles()) {
-			if (!path.isDirectory()) {
-				return false;
-			}
-		}
-		
-		return true;
-	}
-
-	@Override
-	public boolean isFile() {
-		if (parent instanceof MirrorFSRoot)
-	        return false;
-
-		for (File path : getStorageFiles()) {
-			if (!path.isFile()) {
-				return false;
-			}
-		}
-		
-		return true;
-	}
-
-	@Override
-	public boolean doesExist() {
-		for (File path : getStorageFiles()) {
-			if (path.exists()) {
-				return true;
-			}
-		}
-		
-		return false;
-	}
-
-	@Override
-	public boolean isReadable() {
+    @Override
+    public boolean isDirectory() {
+        if (parent instanceof MirrorFSRoot)
+            return true;
+        
+        for (File path : getStorageFiles()) {
+            if (!path.isDirectory()) {
+                return false;
+            }
+        }
+        
         return true;
-	}
+    }
 
-	@Override
-	public boolean isWritable() {
+    @Override
+    public boolean isFile() {
+        if (parent instanceof MirrorFSRoot)
+            return false;
+
+        for (File path : getStorageFiles()) {
+            if (!path.isFile()) {
+                return false;
+            }
+        }
+        
         return true;
-	}
+    }
 
-	@Override
-	public boolean isRemovable() {
-		if (parent instanceof MirrorFSRoot)
-	        return false;
+    @Override
+    public boolean doesExist() {
+        for (File path : getStorageFiles()) {
+            if (path.exists()) {
+                return true;
+            }
+        }
+        
+        return false;
+    }
 
-		return isWritable();
-	}
+    @Override
+    public boolean isReadable() {
+        return true;
+    }
 
-	@Override
-	public String getOwnerName() {
+    @Override
+    public boolean isWritable() {
+        return true;
+    }
+
+    @Override
+    public boolean isRemovable() {
+        if (parent instanceof MirrorFSRoot)
+            return false;
+
+        return isWritable();
+    }
+
+    @Override
+    public String getOwnerName() {
         return parent.getOwnerName();
-	}
+    }
 
-	@Override
-	public String getGroupName() {
+    @Override
+    public String getGroupName() {
         return parent.getGroupName();
-	}
+    }
 
-	@Override
-	public int getLinkCount() {
+    @Override
+    public int getLinkCount() {
         return 1;
-	}
+    }
 
-	@Override
-	public long getLastModified() {
+    @Override
+    public long getLastModified() {
         long lastModified = 0;
         for (File file : storages) {
             if (file.lastModified() > lastModified) {
@@ -142,79 +142,79 @@ public class MirrorFSFile implements FtpFile {
             }
         }
         return lastModified;
-	}
+    }
 
-	@Override
-	public boolean setLastModified(long time) {
-		for (File path : getStorageFiles()) {
-			if (!path.setLastModified(time)) {
-				return false;
-			}
-		}
-		return true;
-	}
+    @Override
+    public boolean setLastModified(long time) {
+        for (File path : getStorageFiles()) {
+            if (!path.setLastModified(time)) {
+                return false;
+            }
+        }
+        return true;
+    }
 
-	@Override
-	public long getSize() {
-		if (!isFile()) {
-			return 0;
-		}
+    @Override
+    public long getSize() {
+        if (!isFile()) {
+            return 0;
+        }
 
         long size = 0;
 
-		for (File file : getStorageFiles()) {
+        for (File file : getStorageFiles()) {
             size = Math.max(size, file.length());
         }
 
-		return size;
-	}
+        return size;
+    }
 
-	@Override
-	public Object getPhysicalFile() {
+    @Override
+    public Object getPhysicalFile() {
         return getAbsolutePath();
-	}
+    }
 
-	public File[] getStorageFiles() {
-		if (storageFiles == null) {
-			storageFiles = new File[storages.length];
+    public File[] getStorageFiles() {
+        if (storageFiles == null) {
+            storageFiles = new File[storages.length];
 
-			for (int i = 0; i < storages.length; i++) {
-				storageFiles[i] = new File(storages[i], getAbsolutePath().substring(1));
-			}
-		}
+            for (int i = 0; i < storages.length; i++) {
+                storageFiles[i] = new File(storages[i], getAbsolutePath().substring(1));
+            }
+        }
 
-		return storageFiles;
-	}
+        return storageFiles;
+    }
 
-	@Override
-	public boolean mkdir() {
-		boolean result = true;
+    @Override
+    public boolean mkdir() {
+        boolean result = true;
 
-		for (File f : getStorageFiles()) {
-			result = result && f.mkdir();
-		}
+        for (File f : getStorageFiles()) {
+            result = result && f.mkdir();
+        }
 
-		return result;
-	}
+        return result;
+    }
 
-	@Override
-	public boolean delete() {
-		boolean result = true;
+    @Override
+    public boolean delete() {
+        boolean result = true;
 
-		for (File f : getStorageFiles()) {
-			result = result && f.delete();
-		}
+        for (File f : getStorageFiles()) {
+            result = result && f.delete();
+        }
 
-		return result;
-	}
+        return result;
+    }
 
-	@Override
-	public boolean move(FtpFile destination) {
-		throw new UnsupportedOperationException("Unimplemented method 'move'");
-	}
+    @Override
+    public boolean move(FtpFile destination) {
+        throw new UnsupportedOperationException("Unimplemented method 'move'");
+    }
 
-	@Override
-	public List<? extends FtpFile> listFiles() {
+    @Override
+    public List<? extends FtpFile> listFiles() {
         LinkedHashMap<String, FtpFile> files = new LinkedHashMap<>();
 
         for (File storage : storages) {
@@ -224,26 +224,26 @@ public class MirrorFSFile implements FtpFile {
         }
 
         return new ArrayList<>(files.values());
-	}
+    }
 
-	@Override
-	public OutputStream createOutputStream(long offset) throws IOException {
-		if (parent instanceof MirrorFSRoot)
-			throw new IOException("Can't create output stream for root directory");
+    @Override
+    public OutputStream createOutputStream(long offset) throws IOException {
+        if (parent instanceof MirrorFSRoot)
+            throw new IOException("Can't create output stream for root directory");
 
-		return new MirrorFileOutputStream(getStorageFiles());
-	}
+        return new MirrorFileOutputStream(getStorageFiles());
+    }
 
-	@Override
-	public InputStream createInputStream(long offset) throws IOException {
-		if (parent instanceof MirrorFSRoot)
-			throw new IOException("Can't create input stream for root directory");
+    @Override
+    public InputStream createInputStream(long offset) throws IOException {
+        if (parent instanceof MirrorFSRoot)
+            throw new IOException("Can't create input stream for root directory");
 
-		return new FileInputStream(getStorageFiles()[RNG.nextInt(storages.length)]);
-	}
+        return new FileInputStream(getStorageFiles()[RNG.nextInt(storages.length)]);
+    }
 
-	public void addStorage(File storage) {
-		storages = Util.arrayAdd(storages, storage);
-	}
+    public void addStorage(File storage) {
+        storages = Util.arrayAdd(storages, storage);
+    }
 
 }
